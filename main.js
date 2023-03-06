8a3bf4b6eb0f8485f380eda86ccd16c7586bec17
Andrz Zack <guillermo07_chavez07@outlook.es>
2023-03-05 20:53:29 -0500

    Realizando el despliegue de las columnas con JS y dandole unos cambios a css

--- main.js
+++ main.js
@@ -2,6 +2,19 @@
 let columnaActiva = 1
 
 //seleccionar columnas
-const columnas = document.querySelector('.columna')
+const columnas = document.querySelectorAll('.columna')
 
-//escuchasr clicks en cada columna
+//escuchar clicks en cada columna
+columnas.forEach((columna, indice) => {
+    columna.addEventListener("click", function() {
+        console.log(columna, indice)
+        cambiarColumna(indice)
+    })    
+});
+
+//cambiar el estado de las columnas
+function cambiarColumna(indice){
+    columnas[columnaActiva].classList.remove("activa")
+    columnas[indice].classList.add("activa")
+    columnaActiva = indice;
+}
